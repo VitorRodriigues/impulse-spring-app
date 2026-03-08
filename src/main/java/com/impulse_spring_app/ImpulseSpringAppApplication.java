5fa2d6edb252906b2a8100bba29ccb5ae75b06be
VitorRodriigues <tdvse@hotmail.com>
2026-03-08 18:02:32 -0300

    Initial commit

---
+++ src/main/java/com/impulse_spring_app/ImpulseSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.impulse_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulseSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulseSpringAppApplication.class, args);
+	}
+
+}
