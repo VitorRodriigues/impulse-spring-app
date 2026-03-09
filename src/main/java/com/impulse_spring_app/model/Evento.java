ba63b6355af70350b737af3c6653f5977d7402d5
VitorRodriigues <tdvse@hotmail.com>
2026-03-09 15:48:44 -0300

    Criação de model e configuração do banco de dados H2

---
+++ src/main/java/com/impulse_spring_app/model/Evento.java
@@ -0,0 +1,31 @@
+package com.impulse_spring_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "eventos")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private LocalDate data;
+
+    private String local;
+
+    private Integer capacidade;
+}
